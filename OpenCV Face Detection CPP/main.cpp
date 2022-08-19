c1da14563f6af64ba88de466104cb9da2daed6af
aallooss <44009215+aallooss@users.noreply.github.com>
2022-08-18 23:55:55 -0400

    added eyes and smile

--- OpenCV Face Detection CPP/main.cpp
+++ OpenCV Face Detection CPP/main.cpp
@@ -11,6 +11,10 @@ int main()
 
 	CascadeClassifier faceCascade;
 	faceCascade.load("C:\\OpenCV4\\etc\\haarcascades\\haarcascade_frontalface_alt.xml");
+	CascadeClassifier eyeCascade;
+	eyeCascade.load("C:\\OpenCV4\\etc\\haarcascades\\haarcascade_eye.xml");
+	CascadeClassifier smileCascade;
+	smileCascade.load("C:\\OpenCV4\\etc\\haarcascades\\haarcascade_smile.xml");
 
 
 	VideoCapture cap(0);
@@ -28,12 +32,28 @@ int main()
 
 		vector<Rect> faces;
 		faceCascade.detectMultiScale(grayscale, faces, 1.1, 3, 0, Size(30, 30));
+		vector<Rect> eye;
+		eyeCascade.detectMultiScale(grayscale, eye, 1.1, 3, 0);
+		vector<Rect> smile;
+		smileCascade.detectMultiScale(grayscale, smile, 1.1, 60, 0, Size(30,30), Size(150,150));
 
 		for (Rect area : faces)
 		{
 			Scalar drawColor = Scalar(255, 0, 0);
 			rectangle(frame, Point(cvRound(area.x * scale), cvRound(area.y * scale)),
 				Point(cvRound((area.x + area.width - 1) * scale), cvRound((area.y + area.height - 1) * scale)), drawColor);
+			for (Rect area : eye)
+			{
+				Scalar drawColor = Scalar(0, 255, 0);
+				rectangle(frame, Point(cvRound(area.x * scale), cvRound(area.y * scale)),
+					Point(cvRound((area.x + area.width - 1) * scale), cvRound((area.y + area.height - 1) * scale)), drawColor);
+			}
+			for (Rect area : smile)
+			{
+				Scalar drawColor = Scalar(0, 0, 255);
+				rectangle(frame, Point(cvRound(area.x * scale), cvRound(area.y * scale)),
+					Point(cvRound((area.x + area.width - 1) * scale), cvRound((area.y + area.height - 1) * scale)), drawColor);
+			}
 		}
 
 		imshow("Webcam Frame", frame);
